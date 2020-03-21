74aea99e9005704e13fe23863883fb5729512503
Chad Carbert <chadcarbert@me.com>
2020-03-20 21:37:49 -0400

    Add patch-auto-unions-interfaces tests

---
+++ tests/unit/mirage/patch-auto-unions-interfaces.test.ts
@@ -0,0 +1,72 @@
+import { patchAutoUnionsInterfaces } from '../../../src/mirage/wrappers/patch-auto-unions-interfaces';
+import { expect } from 'chai';
+import { generateEmptyPackOptions } from '../../mocks';
+import { GraphQLSchema } from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+
+describe('mirage/patch-auto-unions-interfaces', function() {
+  let schema: GraphQLSchema | undefined;
+
+  beforeEach(() => {
+    schema = makeExecutableSchema({
+      typeDefs: `
+        union Salutation = Hello | GutenTag
+
+        type Hello {
+          salutation: String!
+        }
+
+        type GutenTag {
+          salutation: String!
+        }
+
+        interface Animal {
+          type: String!
+        }
+
+        type Dog implements Animal {
+          type: String!
+          breed: String!
+        }
+
+        type Fish implements Animal {
+          type: String!
+          isFreshwater: Boolean!
+        }
+      `,
+    });
+  });
+
+  afterEach(() => {
+    schema = undefined;
+  });
+
+  it('patches missing union and interface __resolveType resolvers', async function() {
+    const resolverMap: any = {};
+    expect(resolverMap?.Salutation?.__resolveType).to.not.exist;
+    expect(resolverMap?.Animal?.__resolveType).to.not.exist;
+
+    const wrapper = patchAutoUnionsInterfaces(schema!);
+    const wrappedResolvers = wrapper(resolverMap!, generateEmptyPackOptions());
+
+    expect(wrappedResolvers?.Salutation?.__resolveType).to.exist;
+    expect(wrappedResolvers?.Animal?.__resolveType).to.exist;
+  });
+
+  it('skips patching already filled union and interface __resolveType resolvers', async function() {
+    const resolverMap: any = {
+      Salutation: {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        __resolveType: () => {},
+      },
+      Animal: {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        __resolveType: () => {},
+      },
+    };
+
+    const wrapper = patchAutoUnionsInterfaces(schema!);
+    const wrappedResolvers = wrapper(resolverMap!, generateEmptyPackOptions());
+    expect(wrappedResolvers).to.deep.equal(resolverMap);
+  });
+});
