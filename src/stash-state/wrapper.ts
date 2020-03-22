d60485fc997d13b8c402606ea83f07f6cdc6045a
Chad Carbert <chadcarbert@me.com>
2020-03-21 20:24:05 -0400

    Add stash-state wrapper

---
+++ src/stash-state/wrapper.ts
@@ -0,0 +1,35 @@
+import { wrapEach } from '../resolver-map/wrap-each';
+import { ResolverMapWrapper } from '../types';
+
+type ResolverStash = {
+  parent: any;
+  args: any;
+  context: any;
+  info: any;
+  result: any;
+};
+
+export const stashKey = Symbol('stash-state');
+export const stashFor = (ref: any): ResolverStash | void => {
+  return ref && ref[stashKey];
+};
+
+export const stashStateWrapper: ResolverMapWrapper = wrapEach(originalResolver => {
+  return (parent, args, context, info) => {
+    const result = originalResolver(parent, args, context, info);
+
+    if (typeof result === 'object' && result !== null) {
+      const stash: ResolverStash = {
+        parent,
+        args,
+        context,
+        info,
+        result,
+      };
+
+      result[stashKey] = stash;
+    }
+
+    return result;
+  };
+});
---
+++ tests/unit/stash-state/wrapper.test.ts
@@ -0,0 +1,34 @@
+import { stashStateWrapper, stashKey, stashFor } from '../../../src/stash-state/wrapper';
+import { pack } from '../../../src/resolver-map/pack';
+import { ResolverMap } from '../../../src/types';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+
+describe('stash-state/wrapper', function() {
+  it('saves stashes on a result object', function() {
+    const resolverReturn = {};
+    const resolverSpy = sinon.spy(() => resolverReturn);
+
+    const resolverMap: ResolverMap = {
+      Query: {
+        rootQueryField: resolverSpy,
+      },
+    };
+
+    const { resolvers: wrappedResolvers } = pack(resolverMap, [stashStateWrapper]);
+
+    const parent = { parent: 'parent' };
+    const args = { args: 'args' };
+    const context = { context: 'context' };
+    const info = { info: 'info' };
+
+    wrappedResolvers.Query.rootQueryField(parent, args, context, info);
+    const stashed = (resolverSpy.firstCall.returnValue as any)[stashKey];
+
+    expect(stashed.parent).to.equal(parent);
+    expect(stashed.args).to.equal(args);
+    expect(stashed.context).to.equal(context);
+    expect(stashed.info).to.equal(info);
+    expect(stashed.result).to.equal(resolverReturn);
+  });
+});
