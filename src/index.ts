c2421d24f98edfd7708d9103f269f25af861bc0f
Chad Carbert <chadcarbert@me.com>
2020-05-17 20:51:43 -0400

    Add index.ts with main package exports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { pack } from './resolver-map/pack';
+export { patchEachField } from './resolver-map/patch-each-field';
+export { wrapEachField } from './resolver-map/wrap-each-field';
+export { embed as embedResolver } from './resolver/embed';
