f12eea0c6f901ba8d11a2d552688d46c61598439
Chad Carbert <chadcarbert@me.com>
2020-08-08 15:59:40 -0400

    Add mirage route handler

---
+++ src/mirage/route-handler.ts
@@ -0,0 +1,25 @@
+import { Response } from 'miragejs';
+import { CreateGraphQLHandlerOptions } from '../graphql/types';
+import { MirageRouteHandler } from './types';
+import { GraphQLHandler } from '..';
+
+export function createRouteHandler(handlerOrOptions: CreateGraphQLHandlerOptions | GraphQLHandler): MirageRouteHandler {
+  const graphqlHandler =
+    handlerOrOptions instanceof GraphQLHandler ? handlerOrOptions : new GraphQLHandler(handlerOrOptions);
+
+  return async function graphQLHandler(_mirageSchema, request): Promise<ReturnType<MirageRouteHandler>> {
+    try {
+      const { query, variables } = JSON.parse(request.requestBody);
+      const result = await graphqlHandler.query(query, variables, { request });
+      return new Response(200, {}, result);
+    } catch (error) {
+      return new Response(
+        500,
+        {},
+        {
+          errors: [JSON.stringify(error)],
+        },
+      );
+    }
+  };
+}
