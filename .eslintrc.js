6cfd054a7810f990150e2a92be65b6e288c9ead5
Chad Carbert <chadcarbert@me.com>
2020-08-18 19:15:43 -0400

    Tweak eslint config a bit more

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
+
+  env: {
+    es2017: true,
+    'shared-node-browser': true,
+  },
 
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "eslint:recommended",
-    "plugin:mocha/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:mocha/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
 
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
 
-  plugins: ["mocha"],
+  plugins: ['mocha'],
 
   rules: {
+    'mocha/no-hooks-for-single-case': 'off'
   },
 };
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: [
-    "../.eslintrc"
-  ],
+  extends: ['../.eslintrc'],
 
   env: {
     mocha: true,
-  }
+    es2017: true,
+    'shared-node-browser': true,
+  },
 };
