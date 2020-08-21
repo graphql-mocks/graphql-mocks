d5bc8b2afb5528e7c7ceb31f55acca058e6f74fe
Chad Carbert <chadcarbert@me.com>
2020-08-20 20:18:42 -0400

    wip

--- src/highlight/index.ts
+++ src/highlight/index.ts
@@ -1,27 +1,93 @@
-import { GraphQLSchema, isSchema } from 'graphql'
+import { GraphQLSchema } from 'graphql';
 
-enum HighlightContext {
-  'TYPES',
-  'FIELDS'
+type Reference = TypeReference | FieldReference;
+type FieldReference = [string, string];
+type TypeReference = string;
+
+interface HighlightMap {
+  [typeName: string]: {
+    fields?: FieldReference[]
+  }
+};
+
+interface Highlighter {
+  (schema: GraphQLSchema, highlighted: HighlightMap): HighlightMap
+};
+
+interface HighlighterFactory {
+  (...options: any[]): Highlighter
+}
+
+const a: HighlighterFactory = () => (_schema, _highlighted) => ({});
+const reference: HighlighterFactory = (...references: Reference[]) => {
+  return (schema, map) {
+    return {};
+  }
 }
 
 class Highlight {
   schema: GraphQLSchema;
+  map: HighlightMap;
 
-  constructor(schema: GraphQLSchema) {
-    if (!isSchema(GraphQLSchema)) throw new Error('schema passed in must be an instance of GraphQLSchema');
+  constructor(schema: GraphQLSchema, map?: HighlightMap = {}) {
+    this.validate(map);
     this.schema = schema;
+    this.map = map;
   }
 
-  include(highlighter) {
+  include(...highlighters: Highlighter[]): Highlight {
+    let schema = this.schema;
+    let map = this.map;
 
+    const combined = highlighters.reduce((map: HighlightMap, highlighter: Highlighter) => {
+      const newMap = highlighter(schema, map);
+      const errors = this.validate(newMap);
+
+      if (errors.length) throw new Error(errors.map(error => error.message).join(', '));
+
+      return newMap;
+    }, map);
+
+    return new Highlight(schema, combined);
+  }
+
+  exclude() {
+
+  }
+
+  filter() {
+
+  }
+
+  // get references() {
+
+  // }
+
+  // get definitions() {
+
+  // }
+
+  validate(map): Error[] {
+    return [];
+  }
+
+  new(map) {
+    return new Highlight(this.schema, map);
   }
 }
 
-interface Highlighter {
-  (options: Record<string, any>) => (schema, references)
+function include(source, new) {
+
 }
 
-export function highlight(schema: GraphQLSchema) {
-  return new Highlight(schema);
+/*
+use(schema)
+with(schema)
+format(schema)
+draft(schema)
+copy(schema)
+*/
+
+function source() {
+
 }
