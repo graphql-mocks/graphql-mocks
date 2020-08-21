136e7b2c6a99fa641aff3ea98b3fee65598f1325
Chad Carbert <chadcarbert@me.com>
2020-08-20 22:23:40 -0400

    partially working with map

--- src/highlight/index.ts
+++ src/highlight/index.ts
@@ -1,63 +1,108 @@
 import { GraphQLSchema } from 'graphql';
+import merge from 'lodash.merge';
+import difference from 'lodash.difference';
+import mergeWith from 'lodash.mergewith';
+import { isArray } from 'util';
 
 type Reference = TypeReference | FieldReference;
 type FieldReference = [string, string];
 type TypeReference = string;
 
 interface HighlightMap {
-  [typeName: string]: {
-    fields?: FieldReference[]
-  }
-};
+  [typeName: string]: string[] | null;
+}
 
 interface Highlighter {
-  (schema: GraphQLSchema, highlighted: HighlightMap): HighlightMap
-};
+  (schema: GraphQLSchema, highlighted: HighlightMap): HighlightMap;
+}
 
 interface HighlighterFactory {
-  (...options: any[]): Highlighter
+  (...options: any[]): Highlighter;
 }
 
-const a: HighlighterFactory = () => (_schema, _highlighted) => ({});
-const reference: HighlighterFactory = (...references: Reference[]) => {
-  return (schema, map) {
-    return {};
-  }
+// const a: HighlighterFactory = () => (_schema, _highlighted) => ({});
+// const reference: HighlighterFactory = (...references: Reference[]) => {
+//   return (schema, map) => {
+//     return {};
+//   }
+// }
+
+/*
+
+export type GraphQLNamedType =
+  | GraphQLScalarType
+  | GraphQLObjectType
+  | GraphQLInterfaceType
+  | GraphQLUnionType
+  | GraphQLEnumType
+  | GraphQLInputObjectType;
+
+*/
+
+export function include(source: HighlightMap, update: HighlightMap): HighlightMap {
+  return mergeWith(source, update, (left, right) => {
+    if (isArray(left) || isArray(right)) {
+      left = left ?? [];
+      right = right ?? [];
+
+      return [...left, ...right];
+    }
+
+    return null;
+  });
 }
 
-class Highlight {
+export function exclude(source: HighlightMap, update: HighlightMap): HighlightMap {
+  return mergeWith(source, update, (sourceValue, updateValue, key, object, source) => {
+    // subtracting a null type reference removes the
+    // type from the source
+    if (updateValue === null) {
+      console.log('here');
+      delete sourceValue[key];
+    }
+
+    if (isArray(sourceValue)) {
+      updateValue = updateValue ?? [];
+
+      return difference(sourceValue, updateValue);
+    }
+
+    return null;
+  });
+}
+
+export class Highlight {
   schema: GraphQLSchema;
   map: HighlightMap;
 
-  constructor(schema: GraphQLSchema, map?: HighlightMap = {}) {
-    this.validate(map);
+  constructor(schema: GraphQLSchema, map?: HighlightMap) {
+    // this.validate(map);
     this.schema = schema;
-    this.map = map;
+    this.map = map ?? {};
   }
 
   include(...highlighters: Highlighter[]): Highlight {
-    let schema = this.schema;
-    let map = this.map;
+    const schema = this.schema;
+    const map = this.map;
 
     const combined = highlighters.reduce((map: HighlightMap, highlighter: Highlighter) => {
       const newMap = highlighter(schema, map);
-      const errors = this.validate(newMap);
+      // const errors = this.validate(newMap);
+      // if (errors.length) throw new Error(errors.map(error => error.message).join(', '));
 
-      if (errors.length) throw new Error(errors.map(error => error.message).join(', '));
-
-      return newMap;
+      return include(map, newMap);
     }, map);
 
     return new Highlight(schema, combined);
   }
 
-  exclude() {
+  // exclude() {
 
-  }
+  // }
 
-  filter() {
+  // filter() {
 
-  }
+  // }
 
   // get references() {
 
@@ -67,27 +112,11 @@ class Highlight {
 
   // }
 
-  validate(map): Error[] {
-    return [];
-  }
+  // validate(map: HighlightMap): Error[] {
+  //   return [];
+  // }
 
-  new(map) {
-    return new Highlight(this.schema, map);
-  }
-}
-
-function include(source, new) {
-
-}
-
-/*
-use(schema)
-with(schema)
-format(schema)
-draft(schema)
-copy(schema)
-*/
-
-function source() {
-
+  // new(map: HighlightMap) {
+  //   return new Highlight(this.schema, map);
+  // }
 }
--- test/unit/highlight/index.test.ts
+++ test/unit/highlight/index.test.ts
@@ -1,7 +1,166 @@
-import {hightlight} from '../../../src/highlight/index';
+import { Highlight, include, exclude } from '../../../src/highlight/index';
+import { buildSchema } from 'graphql';
+import { expect } from 'chai';
 
-describe('highlight', function () {
-  it('accepts a schema') {
-    highlight();
+const schema = buildSchema(`
+  schema {
+    query: Query
   }
-})
+
+  type Query {
+    person: Person!
+  }
+
+  type Person {
+    name: String!
+    age: Int!
+  }
+
+  interface Animal {
+    type: String!
+  }
+
+  type Cat implements Animal {
+    type: String!
+    hasWhiskers: Boolean!
+  }
+
+  type Dog implements Animal {
+    type: String!
+    knowsTricks: Boolean!
+  }
+
+  union FourLeggedAnimal = Cat | Dog
+`);
+
+describe.only('highlight', function () {
+  it('accepts a schema', function () {
+    const h = new Highlight(schema);
+    expect(h.schema).to.equal(schema);
+  });
+
+  it('creates a new instance on include', function () {
+    const h1 = new Highlight(schema);
+    const h2 = h1.include(() => ({
+      Person: ['name'],
+    }));
+
+    expect(h1).to.not.equal(h2);
+  });
+
+  context('#include', function () {
+    it('ignores a null field merging with a field name list', function () {
+      const source = {
+        Person: ['name'],
+      };
+
+      const update = {
+        Person: null,
+      };
+
+      expect(include(source, update)).to.deep.equal({
+        Person: ['name'],
+      });
+    });
+
+    it('can merge lists on the same field name', function () {
+      const source = {
+        Person: ['name'],
+      };
+
+      const update = {
+        Person: ['age'],
+      };
+
+      expect(include(source, update)).to.deep.equal({
+        Person: ['name', 'age'],
+      });
+    });
+
+    it('can merge separate named object with fields', function () {
+      const source = {
+        Person: ['name'],
+      };
+
+      const update = {
+        Cat: ['type'],
+      };
+
+      expect(include(source, update)).to.deep.equal({
+        Person: ['name'],
+        Cat: ['type'],
+      });
+    });
+
+    it('can merge separate named objects with null', function () {
+      const source = {
+        Person: null,
+      };
+
+      const update = {
+        Cat: null,
+      };
+
+      expect(include(source, update)).to.deep.equal({
+        Person: null,
+        Cat: null,
+      });
+    });
+  });
+
+  context('#exclude', function () {
+    it('can remove a value from a field list', function () {
+      const source = {
+        Person: ['name', 'age'],
+      };
+
+      const update = {
+        Person: ['name'],
+      };
+
+      expect(exclude(source, update)).to.deep.equal({
+        Person: ['age'],
+      });
+    });
+
+    it('removes the type when updated by a null-type reference', function () {
+      const source = {
+        Person: ['name', 'age'],
+      };
+
+      const update = {
+        Person: null,
+      };
+
+      expect(exclude(source, update), 'it removes the type reference').to.deep.equal({});
+    });
+
+    it('ignores removing a field list from a null', function () {
+      const source = {
+        Person: null,
+      };
+
+      const update = {
+        Person: ['name', 'age'],
+      };
+
+      expect(exclude(source, update)).to.deep.equal({
+        Person: null,
+      });
+    });
+
+    it('can remove a type entirely', function () {
+      const source = {
+        Person: null,
+      };
+
+      const update = {
+        Person: ['name', 'age'],
+      };
+
+      expect(exclude(source, update)).to.deep.equal({
+        Person: null,
+      });
+    });
+  });
+});
