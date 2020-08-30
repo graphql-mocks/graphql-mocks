c535ecfbba0d3c1763023dec7f70aa328051abfd
Chad Carbert <chadcarbert@me.com>
2020-08-30 09:23:59 -0400

    Add more operation function tests

--- test/unit/highlight/operation/filter.test.ts
+++ test/unit/highlight/operation/filter.test.ts
@@ -3,10 +3,31 @@ import { filter } from '../../../../src/highlight/operation/filter';
 import { Reference } from '../../../../src/highlight/types';
 
 describe('highlight/operation/filter', function () {
-  it('#filter', function () {
+  it('filters the subject from the source', function () {
     const source: Reference[] = ['Person', 'Pet', ['Person', 'name'], ['Person', 'age']];
-    const changes: Reference[] = ['Pet', ['Person', 'name'], 'Bird'];
+    const subject: Reference[] = ['Pet', ['Person', 'name'], 'Bird'];
 
-    expect(filter(source, changes)).to.deep.equal(['Pet', ['Person', 'name']]);
+    expect(filter(source, subject)).to.deep.equal(['Pet', ['Person', 'name']]);
+  });
+
+  it('filters to an empty set when the subject is an empty set', function () {
+    const source: Reference[] = ['Person', 'Pet', ['Person', 'name'], ['Person', 'age']];
+    const subject: Reference[] = [];
+
+    expect(filter(source, subject)).to.deep.equal([]);
+  });
+
+  it('returns an empty source from an empty source', function () {
+    const source: Reference[] = [];
+    const subject: Reference[] = ['Person', 'Pet', ['Person', 'name'], ['Person', 'age']];
+
+    expect(filter(source, subject)).to.deep.equal([]);
+  });
+
+  it('returns an empty source when a filtered set does not overlap with source', function () {
+    const source: Reference[] = ['Person', 'Pet', ['Person', 'name'], ['Person', 'age']];
+    const subject: Reference[] = ['Dog', ['Alien', 'name']];
+
+    expect(filter(source, subject)).to.deep.equal([]);
   });
 });
--- test/unit/highlight/operation/include.test.ts
+++ test/unit/highlight/operation/include.test.ts
@@ -8,4 +8,16 @@ describe('highlight/operation/include', function () {
     const changes: Reference[] = [['Person', 'name']];
     expect(include(source, changes)).to.deep.equal(['Query', ['Person', 'name']]);
   });
+
+  it('returns the same set when an empty set is added', function () {
+    const source: Reference[] = ['Query', ['Person', 'name']];
+    const changes: Reference[] = [];
+    expect(include(source, changes)).to.deep.equal(['Query', ['Person', 'name']]);
+  });
+
+  it('can add to an empty source set', function () {
+    const source: Reference[] = [];
+    const changes: Reference[] = ['Query', ['Person', 'name']];
+    expect(include(source, changes)).to.deep.equal(['Query', ['Person', 'name']]);
+  });
 });
