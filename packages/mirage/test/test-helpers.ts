9dc6a12dade15afeba1551a7481e1adbfdf08ce6
Chad Carbert <chadcarbert@me.com>
2020-09-09 08:46:08 -0400

    Move mirage folder -> @graphql-mocks/mirage

---
+++ packages/mirage/test/test-helpers.ts
@@ -0,0 +1,10 @@
+export const generatePackOptions: (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  mixin?: Record<string, any>,
+) => PackOptions = (mixin = {}) => {
+  return {
+    ...mixin,
+    state: { ...mixin.state },
+    dependencies: { ...mixin.dependencies },
+  };
+};
