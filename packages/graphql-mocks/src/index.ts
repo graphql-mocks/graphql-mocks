a0552f80c22ffc3e8049fcceda5c1e0e9357549a
Chad Carbert <chadcarbert@me.com>
2020-09-13 17:05:11 -0400

    Include named submodule exports from root index

--- packages/graphql-mocks/src/index.ts
+++ packages/graphql-mocks/src/index.ts
@@ -1,3 +1,11 @@
 export { GraphQLHandler } from './graphql';
 export { pack } from './pack';
 export { embed } from './resolver-map';
+export { layer } from './resolver-map';
+
+export * as graphql from './graphql';
+export * as highlight from './highlight';
+export * as relay from './relay';
+export * as resolver from './resolver';
+export * as resolverMap from './resolver-map';
+export * as wrapper from './wrapper';
