2695912799631679d56a8469871e37ef77896d39
Chad Carbert <chadcarbert@me.com>
2020-09-14 21:40:38 -0400

    Working rollup with typescript and babel output

---
+++ packages/graphql-mocks/rollup.config.js
@@ -0,0 +1,72 @@
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+
+import pkg from './package.json';
+
+const external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+
+function modules(format) {
+  let entryFileNames;
+
+  if (format === 'cjs') {
+    entryFileNames = '[name].js';
+  } else if (format === 'es') {
+    entryFileNames = '[name].mjs';
+  } else {
+    entryFileNames = '[name].[format].js';
+  }
+
+  return {
+    input: 'src/index.ts',
+    external,
+
+    output: {
+      format,
+      entryFileNames,
+      dir: 'dist',
+      sourcemap: 'inline',
+    },
+
+    preserveModules: true,
+    plugins: [
+      typescript({ declaration: true, declarationDir: 'dist' }),
+      getBabelOutputPlugin({
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-object-rest-spread',
+        ],
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                node: 10,
+              },
+            },
+          ],
+        ],
+      }),
+    ],
+  };
+}
+
+/*
+function bundle(format) {
+  return {
+    input: 'src/index.ts',
+
+    output: {
+      file: `dist/bundles/graphql-mocks.${format}.js`,
+      format: format,
+      name: 'GraphQLMocks',
+      sourcemap: true,
+    },
+
+    plugins: [typescript()],
+  };
+}
+*/
+
+export default [].concat(modules('cjs'), modules('es') /*, bundle('es'), bundle('cjs'), bundle('umd')*/);
--- packages/graphql-mocks/tsdx.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('../../tsdx.config.js');
