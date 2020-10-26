87f3422106c034e234a6be6a2ec57e457ee6689e
Chad Carbert <chadcarbert@me.com>
2020-10-25 20:04:19 -0400

    Setup typedoc

--- typedoc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  inputFiles: ['./src'],
-  mode: 'modules',
-  out: './documentation/docs/typedoc',
-  plugins: ['markdown'],
-  theme: 'docusaurus2',
-  excludePrivate: true,
-  excludeNotExported: true,
-};
