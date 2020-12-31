b32708969507d021d0eb4e193a66bc23138b9a5d
Chad Carbert <chadcarbert@me.com>
2020-12-31 12:17:16 -0800

    Export mirageCursorForNode from mirage index for use with relay wrapper

--- packages/mirage/src/index.ts
+++ packages/mirage/src/index.ts
@@ -2,3 +2,4 @@ export { createRouteHandler } from './route-handler';
 export { mirageMiddleware } from './middleware';
 export { mirageFieldResolver } from './field-resolver';
 export { mirageTypeResolver } from './type-resolver';
+export { mirageCursorForNode } from './utils';
