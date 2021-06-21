040e86ff5595d6c81c42705c6c861571a0d61677
Chad Carbert <chadcarbert@me.com>
2021-06-21 10:21:05 -0400

    Make hidden symbol document properties not enumerable nor configurable

--- packages/paper/src/document/create-document.ts
+++ packages/paper/src/document/create-document.ts
@@ -9,17 +9,31 @@ export function createDocument<T extends Document = Document>(
   partial: Record<string | symbol, any>,
   key?: string,
 ): T {
-  const document = {
-    ...partial,
+  if (!typename) {
+    throw new Error('The document type name is a required argument');
+  }
 
-    [DOCUMENT_GRAPHQL_TYPENAME]: typename,
+  const document = { ...partial };
 
+  Object.defineProperty(document, DOCUMENT_KEY_SYMBOL, {
+    enumerable: false,
+    configurable: false,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [DOCUMENT_CONNECTIONS_SYMBOL]: partial[DOCUMENT_CONNECTIONS_SYMBOL as any] ?? {},
+    value: partial[DOCUMENT_KEY_SYMBOL as any] ?? key ?? generateDocumentKey(),
+  });
 
+  Object.defineProperty(document, DOCUMENT_GRAPHQL_TYPENAME, {
+    enumerable: false,
+    configurable: false,
+    value: typename,
+  });
+
+  Object.defineProperty(document, DOCUMENT_CONNECTIONS_SYMBOL, {
+    enumerable: false,
+    configurable: false,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [DOCUMENT_KEY_SYMBOL]: partial[DOCUMENT_KEY_SYMBOL as any] ?? key ?? generateDocumentKey(),
-  };
+    value: partial[DOCUMENT_CONNECTIONS_SYMBOL as any] ?? {},
+  });
 
   Object.defineProperty(document, '__typename', {
     configurable: false,
