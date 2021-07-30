89c2ebe60a763836fb6b9b08ee1b93d59c6d0dcb
Chad Carbert <chadcarbert@me.com>
2021-07-30 17:46:12 -0400

    Export document helpers from paper

--- packages/paper/src/index.ts
+++ packages/paper/src/index.ts
@@ -1,4 +1,6 @@
 export { Paper } from './paper';
 export * as operations from './operations';
-export * as types from './types';
 export * as validators from './validations/validators';
+export * as documentHelpers from './document';
+
+export * as types from './types';
