7edc95aed6ffe0a37cae144b54a868fe78d386bb
Chad Carbert <chadcarbert@me.com>
2021-08-20 16:05:44 -0400

    Add semantic pr config and release pr script

---
+++ scripts/create-release.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { execSync } = require('child_process');
+const { env, exit } = require('process');
+
+if (!env.GH_TOKEN) {
+  console.error('Ensure that GH_TOKEN environment variable is set for github release');
+  exit(-1);
+}
+
+const currentBranch = execSync('git branch --show-current').toString().trim();
+if (currentBranch !== 'main') {
+  console.error('Ensure create-release is ran from the main branch');
+  exit(-1);
+}
+
+const cleanWorkingDirectory = execSync('git status --porcelain').toString().trim() === '';
+if (!cleanWorkingDirectory) {
+  console.error('Ensure clean working directory before creating release pr');
+  exit(-1);
+}
+
+try {
+  const commit = execSync('git rev-parse --short HEAD').toString().trim();
+  execSync(`git checkout -b release-${commit}`);
+  execSync(`git push -u origin release-${commit}`);
+
+  execSync('yarn lerna version --conventional-commits --create-release github --yes --force-git-tag', {
+    cwd: process.cwd(),
+    env: process.env,
+  });
+
+  execSync('git add .');
+  execSync(`git commit -m "Creating release from ${commit}"`);
+  execSync(`git push -u origin release-${commit}`);
+} catch (e) {
+  console.log(e.message);
+  exit(-1);
+}
