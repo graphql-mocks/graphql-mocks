7432498f8970a1c536afaf0b019ab89cf398d8a4
Chad Carbert <chadcarbert@me.com>
2021-10-20 12:27:14 -0400

    Check that there are no needed changes to the yarn.lock before creating a release

--- scripts/create-release.js
+++ scripts/create-release.js
@@ -6,13 +6,30 @@ const { exit } = require('process');
 
 const currentBranch = execSync('git branch --show-current').toString().trim();
 if (currentBranch !== 'main') {
-  console.error('Ensure create-release is ran from the main branch');
+  console.error('');
+  console.error('***************************************************');
+  console.error('');
+  console.error(' Ensure create-release is ran from the main branch ');
+  console.error('');
+  console.error('***************************************************');
+  console.error('');
   exit(-1);
 }
 
+// run yarn, if there are changes to yarn.lock then
+// the clean working directory check will fail
+console.log('Running yarn, to ensure no uncommitted changes to yarn.lock');
+execSync('yarn', { stdio: 'ignore' });
+
 const cleanWorkingDirectory = execSync('git status --porcelain').toString().trim() === '';
 if (!cleanWorkingDirectory) {
-  console.error('Ensure clean working directory before creating release pr');
+  console.error('');
+  console.error('******************************************************************');
+  console.error('');
+  console.error(' Error: Ensure clean working directory before creating release pr ');
+  console.error('');
+  console.error('******************************************************************');
+  console.error('');
   exit(-1);
 }
 
