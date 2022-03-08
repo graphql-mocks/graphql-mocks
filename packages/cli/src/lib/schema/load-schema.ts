889665255ac9d187c41750aea7c41a8e276778da
Chad Carbert <chadcarbert@me.com>
2022-03-08 12:35:45 -0500

    Set loadSchema to never cache the results loaded via require

--- packages/cli/src/lib/schema/load-schema.ts
+++ packages/cli/src/lib/schema/load-schema.ts
@@ -1,5 +1,18 @@
 import { buildSchema, GraphQLSchema, buildASTSchema } from 'graphql';
 import { normalizeAbsolutePath } from '../normalize-absolute-path';
+import { resolve } from 'path';
+
+const requireNoCache = function (filePath: string) {
+  const _invalidateRequireCacheForFile = function (filePath: string) {
+    delete require.cache[resolve(filePath)];
+  };
+
+  _invalidateRequireCacheForFile(filePath);
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const contents = require(filePath);
+  _invalidateRequireCacheForFile(filePath);
+  return contents;
+};
 
 export function loadSchema(path: string): { errors: Error[]; path?: string; schema?: GraphQLSchema; type?: string } {
   const filePath = normalizeAbsolutePath(path, { isFile: true });
@@ -15,7 +28,7 @@ export function loadSchema(path: string): { errors: Error[]; path?: string; sche
   let type;
 
   try {
-    contents = require(filePath);
+    contents = requireNoCache(filePath);
     type = typeof contents === 'string' ? 'SDL_STRING' : 'SDL';
     schema = type === 'SDL_STRING' ? buildSchema(contents) : buildASTSchema(contents);
   } catch (e) {
