57135a6aafd5674d5e854c888966cec92a8f1b89
Chad Carbert <chadcarbert@me.com>
2022-05-10 16:24:45 -0400

    Update packages page

--- packages/docs/src/pages/packages.jsx
+++ packages/docs/src/pages/packages.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-function ApiLink({ directoryName, packageName }) {
+function Package({ directoryName, packageName, apiDocsHref }) {
+  apiDocsHref = apiDocsHref ?? `/api/${directoryName}`;
+
   return (
-    <a href={`/api/${directoryName}`}>
-      <code>{packageName}</code>
-    </a>
+    <>
+      <td>
+        <a href={`https://www.npmjs.com/package/${packageName}`}>
+          <code>{packageName}</code>
+        </a>
+      </td>
+      <td>
+        <a href={`https://github.com/graphql-mocks/graphql-mocks/tree/main/packages/${directoryName}`}>
+          <code>/packages/{directoryName}</code>
+        </a>
+      </td>
+      <td>{apiDocsHref ? <a href={apiDocsHref}>API</a> : 'none'}</td>
+    </>
   );
 }
 
@@ -17,40 +29,51 @@ function Packages() {
     <Layout title="Home" description={siteConfig.tagline}>
       <div className="container">
         <header style={{ marginTop: '2rem' }}>
-          <h1>Typescript API Documentation</h1>
+          <h1>Packages &#38; API</h1>
         </header>
         <main>
-          <h2>Packages</h2>
-          <ul>
-            <li>
-              <ApiLink directoryName="graphql-mocks" packageName="graphql-mocks" />
-            </li>
-            <li>
-              <ApiLink directoryName="paper" packageName="graphql-paper" />
-            </li>
-            <li>
-              <ApiLink directoryName="network-msw" packageName="@graphql-mocks/network-msw" />
-            </li>
-            <li>
-              <ApiLink directoryName="network-nock" packageName="@graphql-mocks/network-nock" />
-            </li>
-            <li>
-              <ApiLink directoryName="network-express" packageName="@graphql-mocks/network-express" />
-            </li>
-            <li>
-              <ApiLink directoryName="faker" packageName="@graphql-mocks/faker" />
-            </li>
-            <li>
-              <ApiLink directoryName="sinon" packageName="@graphql-mocks/sinon" />
-            </li>
-            <li>
-              <ApiLink directoryName="mirage" packageName="@graphql-mocks/mirage" />
-            </li>
-          </ul>
+          <table>
+            <thead>
+              <tr>
+                <td>Package Name</td>
+                <td>Package Source</td>
+                <td>API Documentation</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <Package directoryName="graphql-mocks" packageName="graphql-mocks" />
+              </tr>
+              <tr>
+                <Package directoryName="paper" packageName="graphql-paper" />
+              </tr>
+              <tr>
+                <Package directoryName="network-msw" packageName="@graphql-mocks/network-msw" />
+              </tr>
+              <tr>
+                <Package directoryName="network-nock" packageName="@graphql-mocks/network-nock" />
+              </tr>
+              <tr>
+                <Package directoryName="network-express" packageName="@graphql-mocks/network-express" />
+              </tr>
+              <tr>
+                <Package directoryName="cli" packageName="gqlmocks" apiDocsHref="/docs/cli/commands" />
+              </tr>
+              <tr>
+                <Package directoryName="faker" packageName="@graphql-mocks/faker" />
+              </tr>
+              <tr>
+                <Package directoryName="sinon" packageName="@graphql-mocks/sinon" />
+              </tr>
+              <tr>
+                <Package directoryName="mirage" packageName="@graphql-mocks/mirage" />
+              </tr>
+            </tbody>
+          </table>
         </main>
       </div>
     </Layout>
   );
 }
 
-export default Home;
+export default Packages;
