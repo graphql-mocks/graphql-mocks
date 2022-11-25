5898d503163bbd8a3e398244847941e0d08eabab
Chad Carbert <chadcarbert@me.com>
2022-11-25 12:32:23 -0500

    Upgrade docusaurus (#217)

--- packages/docs/babel.config.js
+++ packages/docs/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', require.resolve('@docusaurus/core/lib/babel/preset')],
-  plugins: ['codegen', '@babel/plugin-transform-modules-commonjs'],
+  presets: [['@babel/preset-env', { targets: { node: 16 } }], require.resolve('@docusaurus/core/lib/babel/preset')],
+  plugins: ['codegen'],
 };
