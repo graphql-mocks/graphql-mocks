0eba62960498880ffba7b7d7c2821adb49ed9f42
Chad Carbert <chadcarbert@me.com>
2022-11-25 00:24:17 -0500

    Fix webpack bundling issues by tweaking babel config

--- packages/docs/babel.config.js
+++ packages/docs/babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 16 }, modules: 'commonjs' }],
-    require.resolve('@docusaurus/core/lib/babel/preset'),
-  ],
-  plugins: ['codegen', '@babel/plugin-transform-modules-commonjs'],
+  presets: [['@babel/preset-env', { targets: { node: 16 } }], require.resolve('@docusaurus/core/lib/babel/preset')],
+  plugins: ['codegen'],
 };
