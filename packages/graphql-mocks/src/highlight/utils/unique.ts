3ac572fb05a1e1438cf5534a2d6f5670ee357a37
Chad Carbert <chadcarbert@me.com>
2022-11-29 21:40:55 -0500

    Use isEqual for references instead of ramda equals (#227)

--- packages/graphql-mocks/src/highlight/utils/unique.ts
+++ packages/graphql-mocks/src/highlight/utils/unique.ts
@@ -1,11 +1,11 @@
 import { Reference } from '../types';
-import { equals } from 'ramda';
+import { isEqual } from './is-equal';
 
 export function unique(fieldReferences: Reference[]): Reference[] {
   const uniques: Reference[] = [];
 
   fieldReferences.forEach((reference: Reference) => {
-    const match = uniques.find((uniqueReference) => equals(reference, uniqueReference));
+    const match = uniques.find((uniqueReference) => isEqual(reference, uniqueReference));
     if (!match) uniques.push(reference);
   });
 
