d80f60f59976ff1706d164e1edf5d73e5a44df3d
Chad Carbert <chadcarbert@me.com>
2022-12-18 14:51:48 -0500

    Fix gqlmocks cli dynamic handling of typescript files #231

--- packages/cli/src/hooks/init.ts
+++ packages/cli/src/hooks/init.ts
@@ -4,7 +4,14 @@
 module.exports = async function () {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('@babel/register')({
-    presets: ['@babel/preset-typescript', ['@babel/preset-env', { modules: 'commonjs' }]],
+    presets: ['@babel/preset-typescript', ['@babel/preset-env', { targets: { node: 16 }, modules: 'cjs' }]],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+    // ignore with an empty array is key for allowing transpilation outside of
+    // the current working directory. This is needed because the cli makes
+    // use of putting files in tmp directories and sometimes needs to require
+    // them from there and they might be in a .ts format
+    // see https://github.com/babel/babel/issues/8321
+    ignore: [],
     extensions: ['.js', '.ts'],
   });
 
