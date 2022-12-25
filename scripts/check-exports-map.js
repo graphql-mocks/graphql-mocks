c33ee56bcd29b40f85868d40a49bfc3f80bd4362
Chad Carbert <chadcarbert@me.com>
2022-12-24 23:55:05 -0500

    Add check-exports-map script and modify copy-scrubbed-pjson

--- scripts/check-exports-map.js
+++ scripts/check-exports-map.js
@@ -6,7 +6,11 @@ const path = require('path');
 const { strict: assert } = require('assert');
 const { existsSync } = require('fs');
 
+const INDEX_FILE = 'index.js';
+
 async function checkExportsMap() {
+  console.log('🕵️   package.json exports map check start...');
+
   const { globby } = await import('globby');
   const root = path.resolve(process.cwd());
   const distDir = path.resolve(root, 'dist');
@@ -16,35 +20,53 @@ async function checkExportsMap() {
   assert(existsSync(pjsonFile), 'dist package.json exists');
 
   const pjson = require(pjsonFile);
+  assert.ok(pjson.exports, 'exports property exists in package.json');
+  assert.ok(Object.keys(pjson.exports).length > 0, 'exports map has at least one entry');
 
   let exportMapIndexFiles = [];
   Object.entries(pjson.exports).forEach(([importPath, moduleTypeMap]) => {
     assert.ok(moduleTypeMap.import, `has "import" entry for "${importPath}"`);
     assert.ok(moduleTypeMap.require, `has "require" entry for "${importPath}"`);
-
-    let expectedRequireIndex = path.resolve(distDir, importPath, 'index.js');
-    let expectedImportIndex = path.resolve(distDir, importPath, '/es/', 'index.js');
-
+    let expectedRequireIndex = path.resolve(distDir, importPath, INDEX_FILE);
+    let expectedImportIndex = path.resolve(distDir, 'es', importPath, INDEX_FILE);
     assert.ok(existsSync(expectedRequireIndex), `"require" file ${expectedRequireIndex} exsits at expected location`);
     assert.ok(existsSync(expectedImportIndex), `"import" file ${expectedImportIndex} exsits at expected location`);
 
+    let expectedModuleTypeMapRequire = './' + path.join(importPath, INDEX_FILE);
+    let expectedModuleTypeMapImport = './' + path.join('es', importPath, INDEX_FILE);
+
+    assert.equal(moduleTypeMap.require, expectedModuleTypeMapRequire);
+    assert.equal(moduleTypeMap.import, expectedModuleTypeMapImport);
     exportMapIndexFiles.push(expectedRequireIndex, expectedImportIndex);
   });
 
   const onDiskIndexFiles = await globby(distDir, {
     expandDirectories: {
-      files: ['index.js'],
+      files: [INDEX_FILE],
     },
   });
 
-  assert.deepEqual(
-    exportMapIndexFiles.sort(),
-    onDiskIndexFiles.sort(),
-    'export map index files sand on disk index files match',
+  const missingExportMapIndexFiles = exportMapIndexFiles.filter((file) => !onDiskIndexFiles.includes(file));
+  const missingOnDiskIndexFiles = onDiskIndexFiles.filter((file) => !exportMapIndexFiles.includes(file));
+  const pretty = (obj) => JSON.stringify(obj, null, 2);
+
+  assert.equal(
+    missingExportMapIndexFiles.length,
+    0,
+    `No missing in export map index files:\n${pretty(missingExportMapIndexFiles)}`,
   );
+  assert.equal(
+    missingOnDiskIndexFiles.length,
+    0,
+    `No missing in on disk index files:\n${pretty(missingOnDiskIndexFiles)}`,
+  );
+
+  console.log('🎉  package.json exports map done!');
 }
 
-checkExportsMap().catch(e => {
-  console.error('exports map check failed:');
+checkExportsMap().catch((e) => {
+  console.error('**************************');
+  console.error(' EXPORTS MAP CHECK FAILED ');
+  console.error('**************************');
   console.error(e);
 });
