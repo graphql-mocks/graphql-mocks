66ece5edc4c3c347979408e3391423ddac07d12d
Chad Carbert <2770198+chadian@users.noreply.github.com>
2024-07-01 23:19:25 -0400

    Fix up docs tests

--- packages/docs/scripts/register-babel-for-tests.js
+++ packages/docs/scripts/register-babel-for-tests.js
@@ -1,5 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+console.log('Loading babel/register with codegen plugin for doc tests...');
 require('@babel/register')({
   presets: ['@babel/preset-env'],
   plugins: ['codegen'],
   configFile: false,
+  targets: {
+    node: 'current',
+  },
 });
+console.log('Finished loading babel/register');
