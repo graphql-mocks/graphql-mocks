d0b2a2c96fcffe0c8b6551a20fd6ebffcdc583a9
Chad Carbert <2770198+chadian@users.noreply.github.com>
2024-08-06 22:27:10 -0400

    Reduce iterations over scalar definitions when attaching custom definitions to schema

--- packages/graphql-mocks/src/graphql/utils/attach-scalars-to-schema.ts
+++ packages/graphql-mocks/src/graphql/utils/attach-scalars-to-schema.ts
@@ -1,31 +1,19 @@
-import { GraphQLScalarType, GraphQLSchema, isScalarType } from 'graphql';
+import { GraphQLSchema, isScalarType } from 'graphql';
 import { ScalarMap } from '../../types';
 import { isScalarDefinition } from '../type-utils/is-scalar-definition';
 
 export function attachScalarsToSchema(schema: GraphQLSchema, scalarMap: ScalarMap): void {
-  const scalarTypesArray = Object.values(schema.getTypeMap()).filter(isScalarType);
-  const scalarTypeMap: Record<string, GraphQLScalarType> = scalarTypesArray.reduce((map, scalar) => {
-    return {
-      ...map,
-      [scalar.name]: scalar,
-    };
-  }, {});
+  Object.values(schema.getTypeMap())
+    .filter(isScalarType)
+    .forEach((scalarType) => {
+      const scalarDefinition = scalarMap[scalarType.name];
+      if (!isScalarDefinition(scalarDefinition)) {
+        return;
+      }
 
-  const resolverMapScalars = Object.keys(scalarMap).filter((type) => Object.keys(scalarTypeMap).includes(type));
-
-  for (const scalarName of resolverMapScalars) {
-    const possibleScalar = scalarMap[scalarName];
-
-    if (!isScalarDefinition(possibleScalar)) {
-      continue;
-    }
-
-    const scalarDefinition = possibleScalar;
-    const schemaScalar = scalarTypeMap[scalarName];
-
-    for (const key in scalarDefinition) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (schemaScalar as any)[key] = (scalarDefinition as any)[key];
-    }
-  }
+      for (const key in scalarDefinition) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (scalarType as any)[key] = (scalarDefinition as any)[key];
+      }
+    });
 }
