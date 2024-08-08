26191c283c25a4cf5847ee4d8f2898acf0df9b80
Chad Carbert <2770198+chadian@users.noreply.github.com>
2024-08-08 13:55:22 -0400

    Replace reference filtering with quicker reference intersection helper

---
+++ packages/graphql-mocks/src/highlight/utils/reference-set-methods.ts
@@ -0,0 +1,19 @@
+import { Reference } from '../types';
+import { maskReference, unmaskReference } from './reference-mask';
+
+// TODO: When this library supports node >= 22, replace these with actual Set methods
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set#set_composition
+
+export function referenceIntersection(groupA: Reference[], groupB: Reference[]) {
+  const maskedGroupA = new Set(groupA.map(maskReference));
+  const maskedGroupB = new Set(groupB.map(maskReference));
+
+  const intersection: Reference[] = [];
+  for (const maskedGroupAReference of maskedGroupA) {
+    if (maskedGroupB.has(maskedGroupAReference)) {
+      intersection.push(unmaskReference(maskedGroupAReference));
+    }
+  }
+
+  return intersection;
+}
