4f6f4a11d0e1611995ed28ea15d0580450545dcf
Chad <2770198+chadian@users.noreply.github.com>
2025-01-20 10:53:02 -0500

    Add eslint ignore for any on sequential helper

--- packages/paper/src/hooks/sequential.ts
+++ packages/paper/src/hooks/sequential.ts
@@ -1,5 +1,6 @@
 import { BoundOperationMap, Hook, OperationMap } from '../types';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function sequential<T extends OperationMap>(hooks: Hook<T>[], operations: BoundOperationMap<T>): any[] {
   const results = [];
 
