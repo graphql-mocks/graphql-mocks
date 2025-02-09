045404c89ab5133c00eb258f8eb4df842a37c115
Chad <2770198+chadian@users.noreply.github.com>
2025-02-09 13:08:27 -0500

    Add docusaurus hack to disable prefetch

--- packages/docs/src/pages/index.jsx
+++ packages/docs/src/pages/index.jsx
@@ -18,8 +18,15 @@ const defaultQuery = `{
   }
 }`;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function, valid-typeof
-let Handler = typeof window === 'undefined' ? () => {} : import('../../code-examples/home/handler.source');
+// UGLY HACK:
+// The prefetch functionality of docusaurus is failing and creating console errors.
+// Comment out this line to see the issue. The prefetch attempts to use some internal
+// webpack function that does not exist ("__webpack_require__.u is not a function" when on local dev)
+// The error exists in production builds as well. Since the `prefetch` is more of an optmiziation
+// and the website functions without it this will noop the functions, and `preload` also
+if ('window' in globalThis) {
+  window.docusaurus = { prefetch: () => {}, preload: () => {} };
+}
 
 function Home() {
   const context = useDocusaurusContext();
@@ -151,6 +158,8 @@ function Home() {
                 query={defaultQuery}
                 docExplorerOpen={false}
                 fetcher={async (data) => {
+                  let Handler =
+                    typeof window === 'undefined' ? () => {} : import('../../code-examples/home/handler.source');
                   let graphqlHandler = (await Handler).createHandler();
                   return graphqlHandler.query(data.query).then((result) => {
                     return result;
